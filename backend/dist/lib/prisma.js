0ed57652bbec54ce9766b93f1e51cc6c4faf3770
Dharmendra Singh <dschouhan078@gmail.com>
2026-04-27 09:27:42 +0530

    Fixed .gitignore remove node_modules and dist from tracking

--- backend/dist/lib/prisma.js
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { PrismaPostgresAdapter } from '@prisma/adapter-ppg';
-import { PrismaClient } from "../../generated/prisma/client";
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPostgresAdapter({ connectionString });
-const prisma = new PrismaClient({ adapter });
-export { prisma };
